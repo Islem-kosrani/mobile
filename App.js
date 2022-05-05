aca6e326b4350699faac4cfc177dabfd4424aa39
Nawel Majdoub <nawel.m-ext@fintch.fr>
2022-05-05 22:11:01 +0100

    implements profile screen

--- App.js
+++ App.js
@@ -39,7 +39,7 @@ export default function App() {
 
       
         <Stack.Navigator
-          initialRouteName="StartScreen"
+          initialRouteName="Main"
           screenOptions={{
             headerShown: false,
           }}
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, TouchableOpacity,SafeAreaView } from 'react-native'
+import { View, StyleSheet, TouchableOpacity,SafeAreaView,ScrollView ,Image,Animated, Modal,} from 'react-native'
+import { Icon } from 'react-native-elements';
 
 import {  Avatar,Title,Caption,TouchableRipple,Text } from 'react-native-paper'
 import Background from '../components/Background'
@@ -9,93 +10,171 @@ import Button from '../components/Button'
 import TextInput from '../components/TextInput'
 import BackButton from '../components/BackButton'
 import { theme } from '../core/theme'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import CustumInput from '../components/CustumInput';
+import { color } from 'react-native-reanimated';
+
+const ModalPoup = ({visible, children}) => {
+  const [showModal, setShowModal] = React.useState(visible);
+  const scaleValue = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    toggleModal();
+  }, [visible]);
+  const toggleModal = () => {
+    if (visible) {
+      setShowModal(true);
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      setTimeout(() => setShowModal(false), 200);
+      Animated.timing(scaleValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+  return (
+    
+    <Modal transparent visible={showModal}>
+      <View style={styles.modalBackGround}>
+        <Animated.View
+          style={[styles.modalContainer, {transform: [{scale: scaleValue}]}]}>
+          {children}
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+
 const ProfileScreen =(({ navigation }) =>{
- 
+  const [visible, setVisible] = React.useState(false);
+
  
   
   return (
 
-    <Background>
-      <BackButton goBack={navigation.goBack} />
-      <SafeAreaView style={styles.container}>
-      <View style={styles.userInfoSection}>
-        <View style={{flexDirection:'row',marginTop:15}}>
-          <Avatar.Image
+    <SafeAreaView style={{
+      paddingTop:40,
+  }}>
+        <ScrollView>
+
+    <View style={{
+      backgroundColor:'blue',
+      height:170,
+    }}>
+       <Image
           source={{
-          uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpEwvb5jXV6gwNDK3WzlsNqBm8tm8-m50d4gUxDGtvTSZ-S2OZGvPSHAZ-3NRFD43TEes&usqp=CAU',
-
-        }}
-        size={80}
+            uri:
+              'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png',
+          }}
+          //borderRadius style will help us make the Round Shape Image
+          style={{ width: 50, height: 50, borderRadius: 50 / 2,alignSelf:'center',
+        marginTop:40, }}
         />
-        <View style={{marginLeft:20}}>
-          <Title style={[styles.title,{
-            marginTop:15,
-            marginBottom:5,
-          }]}>Islem</Title>
-          <Caption style={styles.Caption} >@j_doe</Caption>
-        </View>
-        </View>
-      </View>
+      <TouchableOpacity
+                style={{position:'absolute',top:20,right:20}}
 
-      <View style={styles.userInfoSection}>
-<View style={styles.row}>
-  <Icon name='map-marker-radius' color='#5E17EB' size={20}/>
-  <Text style={{ color :'#777777',marginLeft:20 }}>Sousse,Tunisie</Text>
-      </View>
-      <View style={styles.row}>
-  <Icon name='phone' color='#5E17EB' size={20}/>
-  <Text style={{ color :'#777777',marginLeft:20 }}>55121415</Text>
-      </View>
-      <View style={styles.row}>
-  <Icon name='email' color='#5E17EB' size={20}/>
-  <Text style={{ color :'#777777',marginLeft:20 }}>islem@gmail.com</Text>
-      </View>
-    
-      </View>
+      >
+  <Icon 
+          name={'logout'} 
+          type={'ant-design'}
+          color={'#FFFFFF'}
+          size={25}
+          
+          />
+      </TouchableOpacity>
+      <Text style={{
+        textAlign:'center',
+        marginTop:20,
+        color:'white',
+        fontSize:20,
+      }}>Islem Test</Text>
+           <Text style={{
+        textAlign:'center',
+       
+        color:'white',
+        fontSize:16,
+        fontWeight:'bold'
+      }}>Islem@test.com</Text>
+    </View>
+    <CustumInput editable={true} placeholder={"Prénom"} title={'Prénom'}/>
+    <CustumInput editable={true} placeholder={"Nom"} title={'Nom'}/>
+    <CustumInput inputType={'numeric'} editable={true} placeholder={"Téléphone"} title={'Téléphone'}/>
+
+    <CustumInput inputType={'email-address'} editable={true} placeholder={"Email"} title={'Email'}/>
+    <CustumInput onPress={()=>{
+setVisible(true)
+    }}  editable={false} placeholder={"Homme"} title={'Sexe'}/>
+      <TouchableOpacity onPress={() => {}} style={{
+            backgroundColor:'black',
+            padding:10,
+            borderRadius:10,
+            width:'90%',
+            marginBottom:50,
+            alignSelf:'center',
+
+          }}>
+            <Text style={{
+              color:'white',
+              fontWeight:'bold',
+              textAlign:'center',
+            }}>Sauvegarder</Text>
+          </TouchableOpacity>
+  
+  <ModalPoup visible={visible}>
+        <View style={{alignItems: 'center'}}>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => setVisible(false)}>
+              <Image
+                source={require('../assets/X.png')}
+                style={{height: 30, width: 30}}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{alignItems: 'center'}}>
+        
+        </View>
+        <TouchableOpacity onPress={()=>{
+          setVisible(false)
+        }} style={styles.btnSexe}>
+        <Text style={{
+          textAlign:'center',
+          fontWeight:'bold',
+          fontSize:16,
+          color:'white',
+        }}>Homme</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{
+          setVisible(false)
+        }} style={styles.btnSexe}>
+          <Text style={{
+          textAlign:'center',
+          fontWeight:'bold',
+          fontSize:16,
+          color:'white',
+        }}>Femme</Text>
+               </TouchableOpacity>
+
       
+      </ModalPoup>
+      </ScrollView>
 
-      <View style={styles.infoBoxWrapper}>
-        <View style={[styles.infoBox,{
-          borderRightColor:'#dddddd',
-          borderRightWidth:2,
-         
-
-        }]}>
-        <Title> 4 </Title>
-       <Caption>  Nombre des EtoilesEtoies</Caption>
-        </View>
-        <View style={styles.infoBox}>
-        <Title>15</Title>
-        <Caption> Nombre de missions realiseés</Caption>
-      </View>
-      </View>
-      <View style={styles.menuWrapper}>
-        <TouchableRipple   onPress={() => navigation.navigate('ChangermotpasseScreen')}
-        
-        
-        
-        >
-          <View style={styles.menuItem}>
-            <Icon name="update" color='#5E17EB' size={35}/>
-            <Text style ={styles.menuItemText}>Changer Votre Mot passe</Text>
-          </View>
-        </TouchableRipple>
-
-        <TouchableRipple   onPress={() => navigation.navigate('EditProfileScreen')}>
-          <View style={styles.menuItem}>
-            <Icon name="account-settings" color='#5E17EB' size={35}/>
-            <Text style ={styles.menuItemText}> Modifier Votre Compte</Text>
-          </View>
-        </TouchableRipple>
-      </View>
-      </SafeAreaView>
-    </Background>
+    </SafeAreaView>
   );
           });
 
 export default  ProfileScreen;
 const styles = StyleSheet.create({
+  btnSexe:{
+      backgroundColor:'black',
+      padding:10,
+      marginVertical:5,
+  },
   container:{
     flex:1,
   
@@ -147,5 +226,26 @@ justifyContent:'center',
   },
   text:{
     
+  },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+   
+  },
+  modalBackGround: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    width: '80%',
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation: 20,
   },
 })
